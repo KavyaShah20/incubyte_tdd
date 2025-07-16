f8066c88b9ce4c08d69deeab1722c89ed12c8cc8
KavyaShah20 <kavyajayeshshah20@gmail.com>
2025-07-16 20:05:30 +0530

    feat: New sweet addition feature added

--- src/services/sweetshop.service.js
+++ src/services/sweetshop.service.js
@@ -10,7 +10,9 @@ class sweetshop {
         if (!id || !name || !category) {
             throw new Error('Id, name, and category are required');
         }
-    }
 
+        this.sweets[id] = new Sweet(id, name, category, price, quantity);//new sweet added 
+    }
+    
 }
 module.exports = sweetshop;
